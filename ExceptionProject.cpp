ac409a48f788fd8b73b0b61bf94b204c8d209e98
Kelton <kelton.warren2@gmail.com>
2024-12-02 14:42:40 -0500

    Created code outline

---
+++ ExceptionProject.cpp
@@ -0,0 +1,16 @@
+// Kelton ___
+// ________
+// December 2, 2024
+
+#include <cctype>
+
+class invalidCharacterException {};
+class invalidRangeException {};
+
+char character(char, int);
+
+int main() {
+
+
+    return 0;
+}
